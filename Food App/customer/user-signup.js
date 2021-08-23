5493ba9be9255784af3d40bad48d4992eb7bf771
sameirKhan0707 <89413636+sameirKhan0707@users.noreply.github.com>
2021-08-24 00:02:04 +0500

    SMIT Hackathon

---
+++ Food App/customer/user-signup.js
@@ -0,0 +1,66 @@
+let signup = ()=>{
+
+    let username = document.getElementById("username").value
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    let country = document.getElementById("Country").value
+    let city = document.getElementById("City").value
+ 
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in 
+      var user = userCredential.user;
+      console.log("login Successful");
+      // ...
+      firebase.firestore().collection("resturant").doc(user.uid).set({
+         username:username,
+         email:email,
+         uid:user.uid,
+         password:password
+      })
+      .then(function(){
+        console.log("Data Successfull");
+      })
+      .catch(function(error) {
+        console.log(error)
+      })
+      swal({
+       title: "Good job!",
+       text: "Successfully sign up",
+       icon: "success",
+       button: "next",
+    }).then(() =>{
+      location.href ="./login.html"
+    })
+    }) 
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("error",errorMessage)
+      swal("Oops!", errorMessage, "error");
+      // ..
+    });
+   } 
+ 
+ 
+ let login = () => {
+     let email = document.getElementById("login-email").value
+     let password = document.getElementById("login-password").value
+ 
+     // console.log(email.value)
+     // console.log(password.value)
+ 
+     firebase.auth().signInWithEmailAndPassword(email, password)
+   .then((userCredential) => {
+     // Signed in
+     var user = userCredential.user;
+     console.log("Login Successsfully")
+     location.href = ("./dashboard.html")
+     // ...
+   })
+   .catch((error) => {
+     var errorCode = error.code;
+     var errorMessage = error.message;
+     console.log("error",errorMessage)
+   });
+ }
